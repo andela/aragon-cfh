a0a4ac33279dff8409cbc1803a080f5f0fd976f0
Orjiewuru Kingdom <kingdomisaac@ymail.com>
2017-04-20 15:28:49 +0100

    complete invitation feature

--- public/js/controllers/game.js
+++ public/js/controllers/game.js
@@ -31,45 +31,11 @@ angular.module('mean.system')
     }
   };
 
-  $scope.getGameLogs = () => {
-    const userName = window.user.name;
-    $http.get('/api/games/history', { params: { name: userName } })
-        .success((response) => {
-          const userGameLog = response.filter((gameResult) => {
-            if (gameResult.players.indexOf(window.user.name) !== -1) {
-              return gameResult;
-            }
-          });
-          $scope.logs = userGameLog;
-        }, err => console.log(err));
-  };
-
-  $scope.getLeaderBoard = () => {
-    const userName = window.user.name;
-    $http.get('/api/games/leaderboard', { params: { name: userName } })
-        .success((response) => {
-          $scope.boards = response;
-        }, err => console.log(err));
-  };
-
-  $scope.getdonations = () => {
-    const userName = window.user.name;
-    $http.get('/api/games/donations', { params: { name: userName } })
-        .success((response) => {
-          $scope.test = response[0].name;
-          $scope.amount = response[0].donations.length;
-          $scope.donations = response;
-        }, err => console.log(err));
-  };
-
   if (window.user) {
     $scope.isSignedIn = true;
     if (window.user.hideTour) {
       $scope.hideTour = true;
     }
-    $scope.getGameLogs();
-    $scope.getLeaderBoard();
-    $scope.getdonations();
   }
 
   if ($scope.hideTour) {
