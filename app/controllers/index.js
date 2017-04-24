47697552445037678c04b3417963f550e29b2d42
Princess-Jewel Essien <princess-jewel.essien@andela.com>
2017-04-24 08:40:53 +0100

    refactor code

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,21 +1,6 @@
 /**
  * Module dependencies.
  */
-let mongoose = require('mongoose'),
-  async = require('async'),
-  _ = require('underscore');
-
-// /**
-//  * Redirect users to /#!/app (forcing Angular to reload the page)
-//  */
-// exports.play = (req, res) => {
-//   if (Object.keys(req.query)[0] === 'custom') {
-//     res.redirect('/#!/app?custom');
-//   } else {
-//     res.redirect('/#!/app');
-//   }
-// };
-
 exports.render = (req, res) => {
   res.render('index', {
     user: req.user ? JSON.stringify(req.user) : 'null'
